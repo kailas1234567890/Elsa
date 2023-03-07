aa9cf143168be02aea3ddd2b4e70aad5886bd8dc
kailasyt <118808147+kailas1234567890@users.noreply.github.com>
2023-03-07 22:08:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -626,7 +626,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data == "surprise":
         btn = [[
-            InlineKeyboardButton('⌬ Mᴀɪɴ Mᴇɴᴜ ⌬', callback_data='')
+            InlineKeyboardButton('⌬ Mᴀɪɴ Mᴇɴᴜ ⌬', callback_data='start')
         ]]   
         reply_markup=InlineKeyboardMarkup(btn)
         await client.edit_message_media(
